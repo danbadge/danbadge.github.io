f2aefd847311c3c8282861127153e4de03032dfc
Daniel Watts <danbadge@gmail.com>
2017-02-08 21:05:50 +0000

    Publishing to gh-pages

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf public
+git worktree add -B gh-pages public upstream/gh-pages
+hugo
+cd public && git add --all && git commit -m "Publishing to gh-pages" & cd ..
+git push upstream gh-pages
